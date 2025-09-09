7e4ffd14e88e224ea652b1d275de83c1c9b8e7d6
WABarreto <barreto.william@hotmail.com>
2025-09-09 15:44:19 -0300

    Ajuste de pasta

--- dia4/meuprojeto/app2/views.py
+++ dia4/meuprojeto/app2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home2.html")
 
 def renderizar_tela_do_app(request):
-    return render(request, "home2.html")
+    return render(request, "home.html")
